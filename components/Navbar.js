1fdc9021e8ce642bbc3982a5fe9f370a55fc8aae
Rishabh <rrishabhsingh94@gmail.com>
2026-08-17 23:58:45 +0530

    feat: modify Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>
-      I am Navbar
-    </div>
+    <nav className='h-16 bg-purple-700 flex justify-between items-center text-white pxLink'>
+      <div className='logo font-bold text-lg'>
+        Linkly
+      </div>
+      <ul className='flex justify-center items-center gap-4'>
+        <Link href="/"><li>Home</li></Link>
+        <Link href="/about"><li>About</li></Link>
+        <Link href="/generate"><li>Shorten</li></Link>
+        <Link href="/contact"><li>Contact Us</li></Link>
+
+        <li className='flex gap-3'>
+          <Link href="/generate"><button className='bg-purple-500 rounded-lg px-3 py-1 font-bold'>Try Now</button></Link>
+          <Link href="/github"><button className='bg-purple-500 rounded-lg px-3 py-1 font-bold'>Github</button></Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
