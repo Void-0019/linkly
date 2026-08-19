55729582990709cd2b3b08120e2dae9e412194b0
Rishabh <rrishabhsingh94@gmail.com>
2026-08-19 23:54:44 +0530

    feat: add main page content

--- app/page.js
+++ app/page.js
@@ -1,9 +1,29 @@
 import Image from "next/image";
+import localFont from "next/font/local";
+
+
+const poppins = localFont({
+  src: "./fonts/Poppins-Extrabold.ttf",
+  variable: "--font-poppins",
+  weight: "100 900",
+});
 
 export default function Home() {
   return (
-    <main>
-      I am main Page
+    <main className="bg-purple-100">
+      <section className="grid grid-cols-2 h-[50vh]">
+    <div className=" flex flex-col gap-4 items-center justify-center">
+      <p className={`text-3xl font-bold ${poppins.className}`}>
+      The best URL shortener in the Market
+      </p>
+      <p>
+        Transform long URLs into short, powerful links that are easy to share, manage, and remember.
+      </p>
+    </div>
+    <div className=" flex justify-start relative">
+    <Image className="mix-blend-darken" alt="an Image of a vector" src={"/vector.jpg"} fill={true}/>
+    </div>
+      </section>
     </main>
   );
 }
